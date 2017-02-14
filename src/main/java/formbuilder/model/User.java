cc002e6b2735de264a081cdcfac9d6d2174bb374
haiyanwa <haiyanwa@gmail.com>
2017-02-14 13:25:07 -0800

    Change User.java

--- src/main/java/formbuilder/model/User.java
+++ src/main/java/formbuilder/model/User.java
@@ -9,15 +9,7 @@ public class User {
 	private String userFirstName;
 	private String email;
 	private UserAddress address;
-	private boolean role;  //admin:0, user:1
-	private List<DocForm> docs;
-	
-	public User(Integer id, String lname, String fname, String email){
-		this.id = id;
-		this.userLastName = lname;
-		this.userFirstName = fname;
-		this.email = email;
-	}
+	private List<String> roles;  //admin:0, user:1
 	
 	public Integer getId() {
 		return id;
@@ -54,12 +46,6 @@ public class User {
 	}
 	public void setRole(boolean role) {
 		this.role = role;
-	}
-	public List<DocForm> getDocs() {
-		return docs;
-	}
-	public void setDocs(List<DocForm> docs) {
-		this.docs = docs;
 	}
 	
 	
