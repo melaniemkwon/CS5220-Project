f1fe34b1c3532c705433dfe0164c16a349889dfc
melaniemkwon <melaniemkwon@gmail.com>
2017-02-28 12:37:57 -0800

    Added corrected annotations to reflect inheritance in Item, ItemSelection, ItemText models. Also added annotation for Page model.

--- src/main/java/formbuilder/model/Page.java
+++ src/main/java/formbuilder/model/Page.java
@@ -3,10 +3,12 @@ package formbuilder.model;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+@Entity(name = "page")
 public class Page {
 	@Id
     @GeneratedValue
