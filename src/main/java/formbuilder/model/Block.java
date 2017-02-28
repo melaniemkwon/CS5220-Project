34023b7ef31118fa0d043b172bf486b84ecd60a0
melaniemkwon <melaniemkwon@gmail.com>
2017-02-27 22:07:33 -0800

    Added entity and primary key annotations to Block.java model

--- src/main/java/formbuilder/model/Block.java
+++ src/main/java/formbuilder/model/Block.java
@@ -1,8 +1,14 @@
 package formbuilder.model;
 
 import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity(name = "block")
 public class Block {
+	@Id
+    @GeneratedValue
 	private int id;
 	private String name;
 	private String description;
