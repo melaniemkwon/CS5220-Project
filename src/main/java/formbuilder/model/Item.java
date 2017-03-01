bde4f08e5d0e0a728f7aca76c36a4a116fec9536
melaniemkwon <melaniemkwon@gmail.com>
2017-02-28 21:22:57 -0800

    Removed discriminator values from ItemText and ItemSelection

--- src/main/java/formbuilder/model/Item.java
+++ src/main/java/formbuilder/model/Item.java
@@ -3,8 +3,6 @@ package formbuilder.model;
 import java.io.Serializable;
 
 import javax.persistence.Column;
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -14,105 +12,100 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.ManyToOne;
 
-@Entity(name = "item")
+@Entity(name = "Item")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name="item_type")
-@DiscriminatorValue("item")
 public class Item implements Serializable{
-	private static final long serialVersionUID = 1L;
-	
-	@Id
+    private static final long serialVersionUID = 1L;
+    
+    @Id
     @GeneratedValue
-	private double id;
-	private String name;
-	private String description;
-	
-	@Column(name = "orderId")
-	private int orderId; // in which order this item should be shown, when only
-							// by itself the default is 0
-	@ManyToOne
-	private Block block;
-	
-	public enum Type {
-	    TEXT,
-	    RADIOBUTTON,
-	    CHECKBOX,
-	    PULLDOWN
-	}
-	@Enumerated(EnumType.ORDINAL)
-	@Column(name = "type")
-	private Type itemTypes;
-	
-	@Column(name = "required")
-	private boolean isRequired;
-	
-	@ManyToOne
-	private PdfField matchField;
+    private double id;
+    private String name;
+    private String description;
+    
+    @Column(name = "orderId")
+    private int orderId; // in which order this item should be shown, when only
+                            // by itself the default is 0
+    @ManyToOne
+    private Block block;
+    
+    public enum Type {
+        TEXT,
+        RADIOBUTTON,
+        CHECKBOX,
+        PULLDOWN
+    }
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "type")
+    private Type itemTypes;
+    
+    @Column(name = "required")
+    private boolean isRequired;
+    
+    @ManyToOne
+    private PdfField matchField;
 
-	public double getId() {
-		return id;
-	}
+    public double getId() {
+        return id;
+    }
 
-	public void setId(double id) {
-		this.id = id;
-	}
+    public void setId(double id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public int getOrderId() {
-		return orderId;
-	}
+    public int getOrderId() {
+        return orderId;
+    }
 
-	public void setOrderId(int orderId) {
-		this.orderId = orderId;
-	}
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
 
-	public Block getBlock() {
-		return block;
-	}
+    public Block getBlock() {
+        return block;
+    }
 
-	public void setBlock(Block block) {
-		this.block = block;
-	}
+    public void setBlock(Block block) {
+        this.block = block;
+    }
 
-	public boolean isRequired() {
-		return isRequired;
-	}
+    public boolean isRequired() {
+        return isRequired;
+    }
 
-	public void setRequired(boolean isRequired) {
-		this.isRequired = isRequired;
-	}
+    public void setRequired(boolean isRequired) {
+        this.isRequired = isRequired;
+    }
 
-	public Type getItemTypes() {
-		return itemTypes;
-	}
+    public Type getItemTypes() {
+        return itemTypes;
+    }
 
-	public void setItemTypes(Type itemTypes) {
-		this.itemTypes = itemTypes;
-	}
+    public void setItemTypes(Type itemTypes) {
+        this.itemTypes = itemTypes;
+    }
 
-	public PdfField getMatchField() {
-		return matchField;
-	}
+    public PdfField getMatchField() {
+        return matchField;
+    }
 
-	public void setMatchField(PdfField matchField) {
-		this.matchField = matchField;
-	}
-	
-	
-
+    public void setMatchField(PdfField matchField) {
+        this.matchField = matchField;
+    }
 }
--- src/main/java/formbuilder/model/ItemText.java
+++ src/main/java/formbuilder/model/ItemText.java
@@ -1,10 +1,5 @@
 package formbuilder.model;
 
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-
-@Entity(name = "item_text")
-@DiscriminatorValue("text")
 public class ItemText extends Item{
 
 	private String type;
