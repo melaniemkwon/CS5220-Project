b3d99e74e34b2bd71f45284c08bf919b2a4b82e0
cowboyuniverse <cowboyuniverse@gmail.com>
2017-03-01 20:35:18 -0800

    updated itemText.java

--- src/main/java/formbuilder/model/ItemText.java
+++ src/main/java/formbuilder/model/ItemText.java
@@ -12,4 +12,28 @@ public class ItemText extends Item {
 	@Column(name="textLength")
 	private int textLength;
 	
+	
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getInputText() {
+		return inputText;
+	}
+	public void setInputText(String inputText) {
+		this.inputText = inputText;
+	}
+	public int getTextLength() {
+		return textLength;
+	}
+	public void setTextLength(int textLength) {
+		this.textLength = textLength;
+	}
+	
+	
+	
 }
+
