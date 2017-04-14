0f4b7ea2d92482e5a6b20e8dfd71c6eeb610bc91
melaniemkwon <melaniemkwon@gmail.com>
2017-04-14 15:41:51 -0700

    Correct #user.username to returnObject.username

--- src/main/java/formbuilder/model/core/dao/jpa/UserDaoImpl.java
+++ src/main/java/formbuilder/model/core/dao/jpa/UserDaoImpl.java
@@ -20,7 +20,7 @@ public class UserDaoImpl implements UserDao {
 	private EntityManager entityManager;
 
 	@Override
-	@PostAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_STAFF') or principal.username == #user.username")
+	@PostAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_STAFF') or principal.username == returnObject.username")
 	public User getUser(Integer id) {
 		return entityManager.find(User.class, id);
 	}
