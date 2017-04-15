9b9cfa244007372efce58ed92e53677a423b534f
haiyanwa <haiyanwa@gmail.com>
2017-04-15 15:55:53 -0700

    fixed fill form for text

--- src/main/java/formbuilder/web/controller/UserFormController.java
+++ src/main/java/formbuilder/web/controller/UserFormController.java
@@ -26,6 +26,7 @@ import formbuilder.model.questionform.TextAnswer;
 import formbuilder.model.questionform.UserAnswers;
 import formbuilder.model.questionform.dao.AnswerDao;
 import formbuilder.model.questionform.dao.FormDao;
+import formbuilder.security.SecurityUtils;
 
 @Controller
 @SessionAttributes({ "form", "question", "questionsPage","user" })
@@ -52,7 +53,7 @@ public class UserFormController {
 	}
 	
 	@RequestMapping(value="/userForm/fillForm.html", method = RequestMethod.GET)
-	public String viewForm(@RequestParam Integer id, @RequestParam Integer pageNum, ModelMap models){
+	public String viewForm(@RequestParam Integer id, @RequestParam Integer pageNum, @ModelAttribute User user, ModelMap models){
 		
 		Form form = formDao.getForm(id);
 		models.put("form", form);
@@ -62,30 +63,41 @@ public class UserFormController {
 		models.put("questionsPage", questionsPage);
 		UserAnswers userAnswer = new UserAnswers();
 		models.addAttribute("userAnswer", userAnswer);
+		models.put("user", user);
 		
 		return "userForm/viewPage";
 	}
 	
 	@RequestMapping(value="/userForm/fillForm.html", method = RequestMethod.POST)
-	public String fillForm(@ModelAttribute UserAnswers userAnswer, @RequestParam Integer id, @RequestParam Integer pageNum, @ModelAttribute User user, SessionStatus status){
+	public String fillForm(@ModelAttribute UserAnswers userAnswer, @RequestParam Integer id, @RequestParam Integer pageNum, SessionStatus status){
 		ArrayList<String> answers = (ArrayList<String>) userAnswer.getAnswers();
 		Form form = formDao.getForm(id);
 		List<Question> questionsPage = form.getQuestionsPage(pageNum);
 		
+		//loop through every answer
 		for(int i=1;i<=answers.size();i++){
 			for (Question q : questionsPage){
 				//find out which question it's for
 				if(q.getQuestionNumber() == i){
 					if(q.getType().equals("TEXT")){
-						System.out.println("text " + i);
 						TextAnswer ans = new TextAnswer();
 						ans.setText(answers.get(i));
 						ans.setQuestion(q);
+						User user = SecurityUtils.getUser();
 						ans.setUser(user);
 						answerDao.saveAnswer(ans);
 					}else{
-						//ChoiceAnswer ans = new ChoiceAnswer();
-						//ArrayList<ChoiceQuestion> choices = 
+						ChoiceAnswer ans = new ChoiceAnswer();
+						ArrayList<String> selectedChoices = new ArrayList<>();
+						//select 1,2
+						int selectedNum =Integer.parseInt(answers.get(i));
+						ChoiceQuestion cq = (ChoiceQuestion) q;
+						//loop through all choices of this question
+						for(int j=0; j< cq.getChoices().size();j++){
+							if(j == selectedNum){
+								selectedChoices.add(cq.getChoices().get(j));
+							}
+						}
 						//ans.setChoices(choices); 
 						//ans.setSelected(true);
 						//ans.setQuestion(q);
